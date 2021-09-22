afdf0127015e1591b73453fb99ddc3b8f182ce6d
dnapp <nappi.18@osu.edu>
2021-09-21 21:04:51 -0400

    Initial commit
    Initial setup for CRUD backend

---
+++ src/main/java/com/limbrescue/limbrescueangularapp/LimbRescueAngularAppApplication.java
@@ -0,0 +1,13 @@
+package com.limbrescue.limbrescueangularapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimbRescueAngularAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimbRescueAngularAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limbrescue/limbrescueangularapp/LimbRescueAngularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limbrescue.limbrescueangularapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimbRescueAngularAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
