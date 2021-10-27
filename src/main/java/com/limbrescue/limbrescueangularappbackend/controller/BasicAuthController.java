1fd638a64392c69077dc7cc7a9dde4c1ca7a4003
dnapp <nappi.18@osu.edu>
2021-10-26 21:52:51 -0400

    Fixed CrossOrigin tag

--- src/main/java/com/limbrescue/limbrescueangularappbackend/controller/BasicAuthController.java
+++ src/main/java/com/limbrescue/limbrescueangularappbackend/controller/BasicAuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="http://localhost:8081")
 @RestController
 @RequestMapping("/api/v1")
 public class BasicAuthController {
