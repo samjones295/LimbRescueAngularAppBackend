6e75dd93b32e8f66cbb69691f235dcb938d0cb72
yichen2022 <chen.8069@osu.edu>
2021-11-09 21:56:20 -0500

    Tutorial on how to run python ML scripts in java.

--- src/main/java/com/limbrescue/limbrescueangularappbackend/LimbRescueAngularAppBackendApplication.java
+++ src/main/java/com/limbrescue/limbrescueangularappbackend/LimbRescueAngularAppBackendApplication.java
@@ -1,13 +1,16 @@
 package com.limbrescue.limbrescueangularappbackend;
 
+import com.limbrescue.limbrescueangularappbackend.ml.PythonScript;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LimbRescueAngularAppBackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(LimbRescueAngularAppBackendApplication.class, args);
+        PythonScript script = new PythonScript();
+        script.run();
     }
 
 }
---
+++ src/main/java/com/limbrescue/limbrescueangularappbackend/ml/MultiLayerPerceptron.java
@@ -0,0 +1,4 @@
+package com.limbrescue.limbrescueangularappbackend.ml;
+
+public class MultiLayerPerceptron {
+}
