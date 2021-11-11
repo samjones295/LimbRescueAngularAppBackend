88c1d2e67debf150a27fd2f257295c39273e70f1
yichen2022 <chen.8069@osu.edu>
2021-11-11 15:09:52 -0500

    Implemented ML Package.

--- src/main/java/com/limbrescue/limbrescueangularappbackend/LimbRescueAngularAppBackendApplication.java
+++ src/main/java/com/limbrescue/limbrescueangularappbackend/LimbRescueAngularAppBackendApplication.java
@@ -1,8 +1,10 @@
 package com.limbrescue.limbrescueangularappbackend;
 
-import com.limbrescue.limbrescueangularappbackend.ml.PythonScript;
+import com.limbrescue.limbrescueangularappbackend.ml.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class LimbRescueAngularAppBackendApplication {
@@ -12,6 +14,14 @@ public class LimbRescueAngularAppBackendApplication {
         //This is just some tutorial and will be removed.
         PythonScript script = new PythonScript();
         script.run();
+        MultiLayerPerceptron mlp = new MultiLayerPerceptron();
+        mlp.run();
+        NaiveBayes nb = new NaiveBayes();
+        nb.run();
+        RandomForest rf = new RandomForest();
+        rf.run();
+        SupportVectorMachine svm = new SupportVectorMachine();
+        svm.run();
     }
 
 }
--- src/main/java/com/limbrescue/limbrescueangularappbackend/ml/RandomForest.java
+++ src/main/java/com/limbrescue/limbrescueangularappbackend/ml/RandomForest.java
@@ -1,4 +1,26 @@
 package com.limbrescue.limbrescueangularappbackend.ml;
 
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RandomForest {
+    public void run() throws Exception{
+        ProcessBuilder processBuilder = new ProcessBuilder("python", resolvePythonScriptPath("rf.py"));
+        processBuilder.redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+        List<String> results = readProcessOutput(process.getInputStream());
+        System.out.println(results);
+    }
+    private List<String> readProcessOutput(InputStream inputStream) throws IOException {
+        try (BufferedReader output = new BufferedReader(new InputStreamReader(inputStream))) {
+            return output.lines()
+                    .collect(Collectors.toList());
+        }
+    }
+    private String resolvePythonScriptPath(String filename) {
+        File file = new File("src/main/resources/" + filename);
+        return file.getAbsolutePath();
+    }
 }
