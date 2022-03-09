f4909995985d76174fa2e8ef9cb29fb14b28c878
Brian Hartmann <brianhartmann@Brians-MacBook-Pro.local>
2022-03-09 18:30:52 -0500

    cors fix 3

--- src/main/java/com/limbrescue/limbrescueangularappbackend/config/WebSecurityConfig.java
+++ src/main/java/com/limbrescue/limbrescueangularappbackend/config/WebSecurityConfig.java
@@ -2,15 +2,13 @@ package com.limbrescue.limbrescueangularappbackend.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
-public class HTTPSSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .requiresChannel()
-                .anyRequest()
-                .requiresSecure();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 }
