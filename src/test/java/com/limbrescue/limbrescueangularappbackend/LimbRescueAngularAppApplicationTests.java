9cb3ba3aebe124b57cfdee2b2f21e14a334156c4
patton450 <patton.450@osu.edu>
2022-06-30 19:42:55 -0400

    some junit tests

--- src/test/java/com/limbrescue/limbrescueangularappbackend/LimbRescueAngularAppApplicationTests.java
+++ src/test/java/com/limbrescue/limbrescueangularappbackend/LimbRescueAngularAppApplicationTests.java
@@ -1,7 +1,11 @@
 package com.limbrescue.limbrescueangularappbackend;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.limbrescue.limbrescueangularappbackend.model.Reading;
+import com.limbrescue.limbrescueangularappbackend.controller.ReadingDAO;
 
 @SpringBootTest
 class LimbRescueAngularAppBackendApplicationTests {
@@ -10,4 +14,11 @@ class LimbRescueAngularAppBackendApplicationTests {
     void contextLoads() {
     }
 
+
+    @Test
+    void TestReadingNa(){
+        ReadingDAO test = new ReadingDAO();
+        Reading r = test.getReading(-1);
+        assertNull(r);
+    }
 }
